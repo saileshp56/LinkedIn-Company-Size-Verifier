5bc4f99c6d2921cab6c85e7ee439c3a51a2f6014
Sailesh Polavarapu <86889128+saileshp56@users.noreply.github.com>
2023-08-25 10:33:14 -0400

    Create excel_column_to_string_list.py

---
+++ excel_column_to_string_list.py
@@ -0,0 +1,22 @@
+# Open the input file
+# you should copy paste the VNDR_LGL_NM excel column in here
+with open('excel_column.txt', 'r') as file:
+    lines = file.readlines()
+
+
+formatted_lines = set()  # use a set to remove duplicates
+for line in lines:
+    # format the line
+    line = line.strip()
+    # Add quotes around the line to make it a string
+    formatted_line = '"' + line + '",'
+    # Add the formatted line to the set
+    formatted_lines.add(formatted_line)
+
+
+# Write the unique formatted lines to a new file
+with open('excel_column_strings.txt', 'w') as file:
+    file.write("[")
+    file.writelines(formatted_lines)
+    file.write("]")
+
